a97df3aea481146c3b7fa1b77aaa33200a35e732
MR-Stan <michaelrstanley87@gmail.com>
2019-10-18 11:31:51 -0500

    added some javascript structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,48 @@
-// 
+let tollTallyApp = {
+
+    // origin input field related methods
+    originInput : function {
+    // data validation / autocomplete for origin input
+
+    },
+
+    // destination input field related methods
+    destinationInput : function {
+    // data validation / autocomplete for destination input
+
+    },
+
+    // frequency input field related methods
+    frequencyInput : function {
+    // JQueryUI dropdown for frequency
+
+    },
+
+    // total per input field related methods
+    totalPer : function {
+    // JQueryUI calendar for for how long the frequency will occur
+
+    },
+
+    // status bar that shows progress of calculation
+    statusBar : function {
+    // JQueryUI status bar while app is calculating
+
+    },
+
+    // #searchButton on click
+    $("#searchButton").on("click", function{
+
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
